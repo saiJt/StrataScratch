7912e9db602633f339a3b0d933e05c085976e7ef
poeun <95955083+saiJt@users.noreply.github.com>
2026-01-26 23:18:29 -0800

    Refactor shipment date processing and cleaning steps

--- ID2056_Number_of_Shipments_Per_Month/solution.py
+++ ID2056_Number_of_Shipments_Per_Month/solution.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 num_shipments = amazon_shipment[['shipment_id','sub_id','shipment_date']].copy()
 
-num_shipments['shipment_date'] = pd.to_datetime(num_shipments['shipment_date']).dt.strftime('%Y-%m')
+num_shipments['shipment_date'] = pd.to_datetime(num_shipments['shipment_date'])
+                                    .dt.strftime('%Y-%m')
 
-num_cleaned = num_shipments.drop_duplicates().groupby('shipment_date').size().reset_index()
+num_cleaned = num_shipments.drop_duplicates()
+                            .groupby('shipment_date').size().reset_index()
