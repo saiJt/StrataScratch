bccd77e9a52c01c34f7dd806a5d6a5a856b217dc
poeun <95955083+saiJt@users.noreply.github.com>
2026-02-10 22:39:24 -0800

    Add code to find most profitable financial company
    Retrieve the most profitable financial company from the dataset.

---
+++ ID9663_Most_Profitable_Company/solution.py
@@ -0,0 +1,7 @@
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+forbes_global_2010_2014.head()
+
+company = forbes_global_2010_2014[forbes_global_2010_2014['sector'] == 'Financials'].sort_values(by='profits',ascending=False)[['company','continent']].head(1)
