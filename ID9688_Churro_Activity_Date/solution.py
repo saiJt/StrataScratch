7c53ea21e7edac37c741f870b7bca6b78d3fdbbc
poeun <95955083+saiJt@users.noreply.github.com>
2026-02-10 23:47:18 -0800

    Fix formatting of below_stand assignment

--- ID9688_Churro_Activity_Date/solution.py
+++ ID9688_Churro_Activity_Date/solution.py
@@ -5,6 +5,4 @@ import pandas as pd
 los_angeles_restaurant_health_inspections.head()
 dataSet = los_angeles_restaurant_health_inspections
 
-below_stand = dataSet[(dataSet['facility_name']
-                     .str.contains('STREET CHURROS', case = False))&(dataSet['score'] < 95)]
-                     [['activity_date','pe_description']]
+below_stand = dataSet[(dataSet['facility_name'].str.contains('STREET CHURROS', case = False))&(dataSet['score'] < 95)][['activity_date','pe_description']]
