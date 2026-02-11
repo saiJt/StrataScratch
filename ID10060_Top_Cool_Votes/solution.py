0cc4842582319ff67f73b36502a223da4b61dfac
poeun <95955083+saiJt@users.noreply.github.com>
2026-02-11 14:12:53 -0800

    Update solution.py

--- ID10060_Top_Cool_Votes/solution.py
+++ ID10060_Top_Cool_Votes/solution.py
@@ -4,3 +4,7 @@ import pandas as pd
 max_cool = yelp_reviews['cool'].max()
 
 coolest = yelp_reviews[yelp_reviews['cool']== max_cool].sort_values(by='cool',ascending=False)[['business_name','review_text']]
+
+
+# or
+coolest = yelp_reviews[yelp_reviews['cool']== yelp_reviews['cool'].max()].sort_values(by='cool',ascending=False)[['business_name','review_text']]
